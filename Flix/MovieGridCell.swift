a61e8b0fa8bedf92a579d179bcfc95a03010ee5e
Xinyu Xieyang <yx4247@utexas.edu>
2019-02-07 08:09:40 -0600

    06Feb2019

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by 谢阳欣雨 on 2/4/19.
+//  Copyright © 2019 Yangxinyu Xie. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
